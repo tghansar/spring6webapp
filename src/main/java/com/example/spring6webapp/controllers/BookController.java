71d48ef64f12ad0753315dad57c41d7fde2ba748
Taariq Ghansar <tghansar@gmail.com>
2023-07-19 14:00:14 +0200

    Adding the controller to allow reading of books

---
+++ src/main/java/com/example/spring6webapp/controllers/BookController.java
@@ -0,0 +1,28 @@
+package com.example.spring6webapp.controllers;
+
+import com.example.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+
+    /*
+    * Allows the use of multiple implementations using
+    * Spring's Dependency Injection
+    * */
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+
+        return "books";
+    }
+}
